51cbc60791f3164d3cc87fbb9b8308cc7c4e3f49
Alejandro Cavallo <alejandro.cavallo@mail.rockstarcoders.com>
2019-07-19 19:08:52 -0300

    added code to generate thumbnails and compress them

--- handler.js
+++ handler.js
@@ -1,14 +1,31 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
+const convert = require("./lib/convert.js");
+const util = require("util");
+
+module.exports.hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.processFile = async event => {
+  await convert
+    .process(event.Records[0].s3)
+    .then(result => {
+      console.log("Finished convert process");
+      return result;
+    })
+    .catch(err => {
+      console.log("ERROR in handler: %s", err);
+      throw ("Error: %s", err);
+    });
+  return true;
 };
